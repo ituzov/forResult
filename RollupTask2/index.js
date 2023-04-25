6bb7f6bdac2b57d60f1e75495d05c1028afda4f2
ituzov <ilia@ituzov.ru>
2023-04-25 12:30:58 +0300

    Task#2

--- RollupTask2/index.js
+++ RollupTask2/index.js
@@ -1,3 +1,15 @@
+import "./index.css";
+import LOGO from './assets/JavaScript-logo.png';
+
+
+const header = document.createElement('h1')
+header.innerText = 'I love JavaScript'
+
+const img = document.createElement("img");
+img.src = LOGO;
+
+document.body.append(header, img)
+
 console.log('Hello World!');
 const fun = (a,b) => {
     console.log(a,b);
